8b084124a735f67c3f71b77fd006f2aecb6d025e
Guntaporn Pornmanee <klasun12345@gmail.com>
2022-09-11 16:41:15 +0700

    First Commit

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,8 +1,27 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
-  console.log("HI")
-  // esttt
-}
+ 
+  let maxLenght = [];
+  if (arrayOfString === undefined || arrayOfString === null){
+  return undefined
+ }
+ for (const array of arrayOfString){
+   if (maxLenght.length === 0){
+    maxLenght = [array]
+   }
+   else if (array.length >= maxLenght[0].length){
+    if(array.length == maxLenght[0.].length){
+      maxLenght.push(array)
+    }
+    else{
+      maxLenght = [array] 
+    }
+   }
+ }
+
+ return maxLenght
+ } 
+  
+
 module.exports = getMaxLengthString
